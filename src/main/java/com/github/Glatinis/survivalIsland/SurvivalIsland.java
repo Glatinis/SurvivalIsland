52992bf8ff74c5c0560e2ace312dc2f885eadcd3
Glatinis <63927852+Glatinis@users.noreply.github.com>
2026-09-05 11:59:22 +0800

    Initialize

---
+++ src/main/java/com/github/Glatinis/survivalIsland/SurvivalIsland.java
@@ -0,0 +1,16 @@
+package com.github.Glatinis.survivalIsland;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SurvivalIsland extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
